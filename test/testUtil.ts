dcda6226b3775193e29cc6ba4d4f1b96b670e770
Evan Rittenhouse <emanguy1@gmail.com>
2018-08-21 00:06:38 -0400

    Increase docker container startup wait time on tests

--- test/testUtil.ts
+++ test/testUtil.ts
@@ -4,7 +4,7 @@ export async function holdUp(ms:number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export const DOCKER_STARTUP_TIME = 3000;
+export const DOCKER_STARTUP_TIME = 5000;
 export const DEFAULT_ASYNC_TIMEOUT = 10000;
 
 export interface HostPortBinding {
